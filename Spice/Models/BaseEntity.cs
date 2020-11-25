2655f5fae22f7b70032e17ff2dfd8ff06e0afece
czelijm <czelijm@outlook.com>
2020-11-25 18:45:08 +0100

    Divided project to Areas, Added Category Controller, Models, Migrations, Custom.css file, Images

---
+++ Spice/Models/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]// If I want that SQL server generate Id on insert
+        [Key]
+        public Guid Id { get; set; }//If I want that sql server will generate Id
+        //public string Id { get; set; }//If I want generate Id
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public BaseEntity()
+        {
+//            this.Id = Guid.NewGuid().ToString();//If I want generate Id
+            this.CreatedAt = DateTime.Now;
+
+        }
+    }
+}
