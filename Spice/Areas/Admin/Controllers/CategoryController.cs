0fbfcbbbf47a47598eade7e6ff28238eb1c4a7b1
czelijm <czelijm@outlook.com>
2020-12-10 00:31:40 +0100

    Added Delete Post Route, Details Get Route And View, Add few Partial Views

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -37,12 +37,12 @@ namespace Spice.Areas.Admin.Controllers
         //POST - Create
         [HttpPost]
         [ValidateAntiForgeryToken]//to prevent attacs
-        public async Task<IActionResult> Create(Category category) 
+        public async Task<IActionResult> Create(Category category)
         {
             if (ModelState.IsValid)
             {
                 await _db.Category.AddAsync(category);
-                await _db.SaveChangesAsync(); 
+                await _db.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index)); // return to action method that call the view
             }
@@ -51,7 +51,7 @@ namespace Spice.Areas.Admin.Controllers
         }
 
         //GET - Edit
-        public async Task<IActionResult> Edit(Guid? id) 
+        public async Task<IActionResult> Edit(Guid? id)
         {
             if (id == null)
             {
@@ -59,7 +59,7 @@ namespace Spice.Areas.Admin.Controllers
             }
 
             var result = await _db.Category.FindAsync(id);
-            if (result==null)
+            if (result == null)
             {
                 return NotFound();
             }
@@ -69,7 +69,7 @@ namespace Spice.Areas.Admin.Controllers
         //POST action method
         [HttpPost]
         [ValidateAntiForgeryToken]//to prevent attacks
-        public async Task<IActionResult> Edit(Category category) 
+        public async Task<IActionResult> Edit(Category category)
         {
             if (ModelState.IsValid)
             {
@@ -83,29 +83,45 @@ namespace Spice.Areas.Admin.Controllers
         //Name of the argument must have this same name as the asp-route-Name
         public async Task<IActionResult> Delete(Guid? id)
         {
-            if (id==null)
-            {   
+            if (id == null)
+            {
                 return NotFound();
             }
             var result = await _db.Category.FindAsync(id);
-            if (result==null)
+            if (result == null)
             {
                 return NotFound();
             }
             return View(result);
         }
 
-        [HttpDelete]
+        // [HttpPost,ActionName("Delete")] 
+        [HttpPost, ActionName("Delete")] //for action method name DeleteConfirm, becouse there cannot exist action methods with this same name
+                                         //It has to be the HttpPost not the HttpDelete
         [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> Delete(Category category) 
+        public async Task<IActionResult> DeletePostAction(Guid id)
         {
-            if (ModelState.IsValid)
+            var result = await _db.Category.FindAsync(id);
+
+            if (result == null)
             {
-                _db.Category.Remove(category);
-                await _db.SaveChangesAsync();
+                return NotFound();
+            }
+
+            _db.Category.Remove(result);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Details(Guid id) 
+        {
+            var result = await _db.Category.FindAsync(id);
+            if (result==null)
+            {
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(result);
         }
 
         //POST action method
@@ -133,6 +149,5 @@ namespace Spice.Areas.Admin.Controllers
 
         //    return View(Category);
         //}
-
     }
 }
