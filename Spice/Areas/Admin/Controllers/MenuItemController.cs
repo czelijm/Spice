5e48a1b866c93ab81b740fd90393d522a3073b71
czelijm <czelijm@outlook.com>
2020-12-24 01:23:41 +0100

    Added MenuItem Model; Added Migrations, Added MenuItemViewModel; Added MenuItemController, use BindProperty;  Added Get Index and Get Create Routes;

---
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Spice.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Spice.Models.ViewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment; // this is in config method in startup.cs as parameter
+
+        [BindProperty] //from .net core 3.1 we can use bind property instead of passing MenuItemViewModel as argument to every action method
+        public MenuItemViewModel MenuItemViewModel { get; set; }
+
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+            MenuItemViewModel = new MenuItemViewModel
+            {
+                MenuItem = new Models.MenuItem(),
+                CategoryList = _db.Category,
+                //SubCategoryList = await _db.SubCategory.ToListAsync()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _db.MenuItem.Include(s=> s.Category).Include(s=>s.SubCategory).ToListAsync();
+            return View(result);
+        }
+
+        //GET - Create
+        public async Task<IActionResult> Create()
+        {
+            return View(MenuItemViewModel);
+        }
+    }
+}
---
+++ Spice/Models/MenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class MenuItem : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Spicyness { get; set; }
+        public enum ESpicy 
+        {
+            NA=0,
+            NotSpicy=1,
+            Spicy=2,
+            VerySpicy=3
+        }
+        //see if the images is in the servers, images are on the servers, in db we store only paths
+        public string Image { get; set; }
+        [Display(Name="Category")]
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+        [Display(Name = "SubCategory")]
+        public Guid SubCategoryId { get; set; }
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+
+        [Range(1,int.MaxValue, ErrorMessage = "Price should be greater than ${1}")]
+        public double Price { get; set; }
+    }
+}
