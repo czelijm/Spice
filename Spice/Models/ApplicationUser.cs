8546db254563b6275ad20892fa447831b7048858
czelijm <czelijm@outlook.com>
2021-01-03 21:49:42 +0100

    Extend application User Dodel

---
+++ Spice/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string StreetAddres { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+    }
+}
