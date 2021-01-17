246b883a06c6b895f222c343e884f2aba72e241e
czelijm <czelijm@outlook.com>
2021-01-17 01:46:53 +0100

    Added CartController, Shopping cart's Index Page template

---
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        private OrderDetailsCartViewModel OrderDetailsCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+            OrderDetailsCart = new OrderDetailsCartViewModel();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            OrderDetailsCart = new OrderDetailsCartViewModel()
+            {
+                OrderHeader = new Models.OrderHeader(),
+            };
+
+            OrderDetailsCart.OrderHeader.OrderTotalDiscount = "0";
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim==null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var cart = await _db.ShoppingCart.Where(c => c.ApplicationUserId.ToString().Equals(claim.Value)).ToListAsync();
+
+            if (cart!=null)
+            {
+                OrderDetailsCart.ListCart = cart.ToList(); 
+                    
+            }
+
+            foreach (var list in OrderDetailsCart.ListCart)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m=>m.Id.ToString().Equals(list.MenuItemId.ToString()));
+                decimal orderTotal = decimal.Parse(OrderDetailsCart.OrderHeader.OrderTotalDiscount);
+                orderTotal+=(decimal)(list.MenuItem.Price*list.Count);
+                OrderDetailsCart.OrderHeader.OrderTotalDiscount = orderTotal.ToString();
+                list.MenuItem.Description = Utility.SD.ConvertToRawHtml(list.MenuItem.Description);
+                if (list.MenuItem.Description.Length>100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+            OrderDetailsCart.OrderHeader.OrderTotalOriginal = OrderDetailsCart.OrderHeader.OrderTotalDiscount;
+
+            return View(OrderDetailsCart);
+        }
+    }
+}
--- Spice/Utility/SD.cs
+++ Spice/Utility/SD.cs
@@ -19,7 +19,36 @@ namespace Spice.Utility
 
         //public static string SessionCountCookie = "ssCount";
         public static string SessionCartCountCookie = "ssCartCount";
-    }
+
+		public static string ConvertToRawHtml(string source)
+		{
+			char[] array = new char[source.Length];
+			int arrayIndex = 0;
+			bool inside = false;
+
+			for (int i = 0; i < source.Length; i++)
+			{
+				char let = source[i];
+				if (let == '<')
+				{
+					inside = true;
+					continue;
+				}
+				if (let == '>')
+				{
+					inside = false;
+					continue;
+				}
+				if (!inside)
+				{
+					array[arrayIndex] = let;
+					arrayIndex++;
+				}
+			}
+			return new string(array, 0, arrayIndex);
+		}
+
+	}
 
 }
 
